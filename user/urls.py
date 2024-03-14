ea0ccb730c591c5dc039b58b089b9cfb02f8b29d
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-03-14 22:34:08 +0800

    Added logout & login feature

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 
 
 urlpatterns = [
-    path('',  TemplateView.as_view(template_name="user/home.html")),
-    path('login',auth_views.LoginView.as_view(template_name="user/login.html"))    
+    path('',  TemplateView.as_view(template_name="user/home.html"), name="home"),
+    path('login',
+         auth_views.LoginView.as_view(
+             template_name="user/login.html",
+             redirect_authenticated_user=True),
+         name="login"),
+    path('logout', auth_views.LogoutView.as_view(), name="logout"),
+    path('accounts/', include('django.contrib.auth.urls')),    
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from user.forms import RegistrationForm
 
-# Create your views here.
 
 
+# def sign_up(request):
+#     if request.method == "POST":
+#         form = RegistrationForm(request.post)
+#         if form.is_valid():
+#             user = 
